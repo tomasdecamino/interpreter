3d7d9b592123bc92c0ccc2ba8ce2c6a398158cc0
Tomás de Camino Beck, Ph.D <tomas.decamino@gmail.com>
2022-10-14 07:49:25 -0600

    Add files via upload

---
+++ interpreter.py
@@ -0,0 +1,14 @@
+class interpreter:
+    def __init__(self):
+        self.functionDict = {}
+         
+        
+    def addFunction(self,name,f):
+        self.functionDict.update({name:f})
+        
+    def execute(self,commands):
+        for com in commands.split(':'):
+            try:
+                self.functionDict[com][0](self.functionDict[com][1])
+            except:
+                print('function not found')
---
+++ main.py
@@ -0,0 +1,28 @@
+from roboboard import RoboBoard
+from interpreter import *
+from time import sleep_ms
+
+rb = RoboBoard()
+inter = interpreter()
+
+#define functions (using non just to get a None parameter)
+def redColor(non):
+    rb.pixel((255,0,0))
+    
+def blueColor(non):
+    rb.pixel((0,0,255))
+    
+def noColor(non):
+    rb.pixel((0,0,0))
+
+# add functions to interpreter ('name', (function, parameter))
+inter.addFunction('sleep1000',(sleep_ms,1000))
+inter.addFunction('sleep100',(sleep_ms,100))
+inter.addFunction('redColor',(redColor,None))
+inter.addFunction('noColor',(noColor,None))
+inter.addFunction('blueColor',(blueColor,None))
+
+
+
+while True:
+    inter.execute('redColor:sleep1000:noColor:sleep100:blueColor:sleep1000')
